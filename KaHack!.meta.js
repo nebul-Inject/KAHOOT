f1e0f7b9ad3ad70135384d77ce602aab3da31888
IMPERIAL <jescamvinted@gmail.com>
2025-03-25 12:29:26 +0100

    Add files via upload

---
+++ KaHack!.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         KaHack!
+// @version      1.0.25
+// @namespace    https://github.com/jokeri2222
+// @description  A hack for kahoot.it!
+// @updateURL    https://github.com/jokeri2222/Kahoot-Hack/raw/main/KaHack!.meta.js
+// @downloadURL  https://github.com/jokeri2222/Kahoot-Hack/raw/main/KaHack!.user.js
+// @author       jokeri2222; https://github.com/jokeri2222
+// @match        https://kahoot.it/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=kahoot.it
+// @grant        none
+// ==/UserScript==
